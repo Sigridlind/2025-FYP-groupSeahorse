1169c9ad8766a2cbed356e8278d7dcc72715a914
Emil Korczak <ekor@itu.dk>
2025-05-25 15:03:25 +0200

    Changed main_baseline.py and add classifier.py

--- main_baseline.py
+++ main_baseline.py
@@ -6,11 +6,13 @@ import pandas as pd
 import util.feature_A
 import util.feature_B
 import util.feature_C
+import util.classifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.model_selection import train_test_split
 from util.img_util import readImageFile, saveImageFile
 from util.inpaint_util import removeHair
+from imblearn.over_sampling import SMOTE
 
 
 def main(csv_path, mask_path, img_path, save_path):
@@ -52,14 +54,15 @@ def main(csv_path, mask_path, img_path, save_path):
     df["feat_C"] = feat_C_values
     df["feat_D"] = int(round((df["diameter_1"] + df["diameter_2"]) / 2, 0))
     df["feat_E"] = df["grew"] == "True" or df["changed"] == "True"
-
-    # # Gem udvalgt data
-    # df_out = df[["img_id", "feat_A", "feat_B", "label"]]
-    # df_out.to_csv(save_path, index=False)
-    # print(f"Features gemt i: {save_path}")
-
     df['label'] = df["diagnostic"] == "MEL"
+     
+    # Gem udvalgt data
+    df_out = df[["img_id", "feat_A", "feat_B","feat_C", "feat_D", "feat_E", "label"]]
+    df_out.to_csv(save_path, index=False)
 
+def classification(feature_csv_path, results_path):
+    df = pd.read_csv(feature_csv_path)
+    
     # select only the baseline features.
     baseline_feats = [col for col in df.columns if col.startswith("feat_")]
     x_all = df[baseline_feats]
@@ -71,23 +74,31 @@ def main(csv_path, mask_path, img_path, save_path):
     # split testing sets into 15% val, 15% test
     x_val, x_test, y_val, y_test = train_test_split(x_temp, y_temp, test_size=0.5, random_state=17)
 
-    # # train the classifier (using logistic regression as an example)
-    # clf = LogisticRegression(max_iter=1000, verbose=1)
-    # clf.fit(x_train, y_train)
+    # Apply SMOTE to training set
+    smote = SMOTE(random_state=17)
+    x_train_res, y_train_res = smote.fit_resample(x_train, y_train)
+    
+    # Tune models using validation set
+    tuned_results = util.classifier.tune_models(x_train_res, y_train_res, x_val, y_val)
+    
+    final_results = {}
+    for name, res in tuned_results.items():
+        print(f"\n{name} Validation F1: {res['f1']:.3f}, AUC: {res['auc']:.3f}, Accuracy: {res['acc']:.3f}, Precision: {res['precision']:.3f}, Recall: {res['recall']:.3f}")
+        
+    best_model_name = max(tuned_results, key=lambda name: tuned_results[name]["f1"])
+    best_model = tuned_results[best_model_name]["model"]
+    print(f"Best model is (Based on validation performance): {best_model_name}")
+    
+    final_result = util.classifier.evaluate_model(best_model, x_test, y_test)
+    print(f"\n{best_model_name} Test F1: {final_result['f1']:.3f}, AUC: {final_result['auc']:.3f}, Accuracy: {final_result['acc']:.3f}, Precision: {final_result['precision']:.3f}, Recall: {final_result['recall']:.3f}")
+    print("Confusion Matrix:\n", final_result["cm"])
 
-    # # test the trained classifier
-    # y_pred = clf.predict(x_test)
-    # acc = accuracy_score(y_test, y_pred)
-    # cm = confusion_matrix(y_test, y_pred)
-    # print("Test Accuracy:", acc)
-    # print("Confusion Matrix:\n", cm)
-
-    # # write test results to CSV.
-    # result_df = data.loc[X_test.index, ["filename"]].copy()
-    # result_df['true_label'] = y_test.values
-    # result_df['predicted_label'] = y_pred
-    # result_df.to_csv(save_path, index=False)
-    # print("Results saved to:", save_path)
+    # write test results to CSV.
+    df_out = df.loc[x_test.index, ["img_id"]].copy()
+    df_out["true_label"] = y_test.values
+    df_out["predicted_label"] = final_result["y_pred"]
+    df_out["melanoma_probability"] = final_result["y_prob"]
+    df_out.to_csv(results_path, index=False)
 
 
 if __name__ == "__main__":
---
+++ util/classifier.py
@@ -0,0 +1,43 @@
+import numpy as np
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score, confusion_matrix, roc_auc_score
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+
+def tune_models(x_train, y_train, x_val, y_val):
+    results = {}
+
+    # KNN
+    knn_parameters = [3, 5, 7, 9]
+    knn = GridSearchCV(KNeighborsClassifier(), knn_parameters, scoring='f1')
+    knn.fit(x_train, y_train)
+    knn_best = knn.best_estimator_
+    results["KNN"] = evaluate_model(knn_best, x_val, y_val)
+
+    # Decision Tree
+    dt_params = [3, 5, 10, None]
+    dt = GridSearchCV(DecisionTreeClassifier(random_state=17), dt_params, scoring='f1')
+    dt.fit(x_train, y_train)
+    dt_best = dt.best_estimator_
+    results["DecisionTree"] = evaluate_model(dt_best, x_val, y_val)
+
+    # Random Forest
+    rf_params = [3, 5, 10, None]
+    rf = GridSearchCV(RandomForestClassifier(random_state=17), rf_params, scoring='f1')
+    rf.fit(x_train, y_train)
+    rf_best = rf.best_estimator_
+    results["RandomForest"] = evaluate_model(rf_best, x_val, y_val)
+
+    return results
+
+def evaluate_model(model, x, y):
+    y_pred = model.predict(x)
+    y_prob = model.predict_proba(x)[:, 1]
+    acc = accuracy_score(y, y_pred)
+    recall = recall_score(y, y_pred)
+    precision = precision_score(y, y_pred)
+    f1 = f1_score(y, y_pred)
+    auc = roc_auc_score(y, y_prob)
+    cm = confusion_matrix(y, y_pred)
+    return {"model": model,"acc": acc,"recall": recall, "precision": precision,"f1": f1, "auc": auc, "cm": cm, "y_pred": y_pred, "y_prob": y_prob}
